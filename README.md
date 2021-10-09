74d2b8c297ea764c48bbd9fe32d938299966b407
hackwebdev <hackwebdev@gmail.com>
2021-10-10 02:43:22 +0800

    add header link

--- README.md
+++ README.md
@@ -22,3 +22,36 @@ Fix
 ```
 Delete the .next folder at the root of your project, relaunch your project, and force-refresh your page (Shift+F5 / Cmd+Shift+R) to remove the cache.
 ```
+
+# Next.js Amazona
+
+Build ECommerce Website Like Amazon by Next.js
+
+## Lessons
+
+1. Introduction
+   1. What you will learn
+   2. What you will build
+   3. What Packages you will use
+2. Install Tools
+   1. VS Code
+   2. Chrome
+   3. Node.js
+   4. MongoDB
+3. Create Next App
+   1. npx create-next-app
+   2. add layout component
+   3. add header, main and footer
+4. Add Styles
+   1. add css to header, main and footer
+5. Fix SSR Issue on MaterialUI
+   1. add \_documents.js
+   2. add code to fix styling issue
+6. List Products
+   1. add data.js
+   2. add images
+   3. render products
+7. Add header links
+   1. Add cart and login link
+   2. use next/link and mui/link
+   3. add css classes for header links
--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
-import { AppBar, Toolbar, Typography, Container } from '@material-ui/core'
+import NextLink from 'next/link'
+import { AppBar, Toolbar, Typography, Container, Link } from '@material-ui/core'
 import useStyles from '../utils/styles'
 
 export default function Layout({ children }) {
@@ -8,16 +9,29 @@ export default function Layout({ children }) {
   return (
     <div>
       <Head>
-        <title>Tatak RGB</title>
+        <title>Next Amazona</title>
       </Head>
       <AppBar position='static' className={classes.navbar}>
         <Toolbar>
-          <Typography>Tatak RGB</Typography>
+          <NextLink href='/' passHref>
+            <Link>
+              <Typography className={classes.brand}>amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href='/cart' passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href='/login' passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
       <footer className={classes.footer}>
-        <Typography>All rights reserved. Tatak RGB.</Typography>
+        <Typography>All rights reserved. Next Amazona.</Typography>
       </footer>
     </div>
   )
